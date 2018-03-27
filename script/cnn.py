8dd819a1a6021aeef211fc2a5472a2bc48374e83
pointW <199410075@qq.com>
2018-03-27 19:03:09 -0400

    completed simple training

--- script/cnn.py
+++ script/cnn.py
@@ -1,8 +1,25 @@
+# python
+import os
+
+# torch
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+
+# numpy
+import numpy as np
+
+f = os.path.dirname(__file__) + '/../data/train_normal_1.npy'
+data_normal = np.load(f)
+np.random.shuffle(data_normal)
+test_normal, training_normal = data_normal[:20, :], data_normal[20:, :]
+f = os.path.dirname(__file__) + '/../data/train_edge_1.npy'
+data_edge = np.load(f)
+test_edge, training_edge = data_edge[:20, :], data_edge[20:, :]
+DATA = map(lambda x: [x, 0], training_normal) + map(lambda x: [x, 1], training_edge)
+np.random.shuffle(DATA)
 
 
 class CNN(nn.Module):
@@ -22,7 +39,7 @@ class CNN(nn.Module):
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.softmax(self.fc3(x))
+        x = F.softmax(self.fc3(x), dim=1)
         return x
 
     def num_flat_features(self, x):
@@ -34,7 +51,26 @@ class CNN(nn.Module):
 
 
 cnn = CNN()
-criterion = nn.CrossEntropyLoss()
+criterion = F.cross_entropy
 optimizer = optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
 
 
+for epoch in range(2):
+    running_loss = 0.0
+    for i, data in enumerate(DATA):
+        inputs, labels = data
+        inputs, labels = torch.FloatTensor(inputs), torch.LongTensor([labels])
+        inputs = torch.unsqueeze(inputs, 0)
+        inputs, labels = Variable(inputs), Variable(labels)
+        optimizer.zero_grad()
+        outputs = cnn(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.data[0]
+        if i % 20 == 19:
+            print '[%d, %d] loss: %.3f' % (epoch+1, i+1, running_loss / 20)
+            running_loss = 0.0
+
+print 'finished'
