ee49b5c7a3828338d51e4783c0c56e84e04bea88
pointW <199410075@qq.com>
2018-03-29 18:38:42 -0400

    changed way to choose grasp

--- script/create_training_set.py
+++ script/create_training_set.py
@@ -21,13 +21,17 @@ from hand_descriptor import HandDescriptor
 import point_cloud
 
 
-workspace = [(-1.0, 1.0), (0, 1), (-0.50, 0.50)]
-# edge grasp
+workspace = [(-0.5, 1.0), (0, 1.5), (-0.50, 0.50)]
+# edge graspgitr
 # p = [-0.1, 0.75, -0.2]
 # grasp_space = [(p[0]-0.5, p[0]+0.5), (p[1]-0.5, p[1]), (p[2]-0.1, p[2]+0.05)]
 
-p = [-0.09, 0.61, -0.15]
-grasp_space = [(p[0]-0.05, p[0]+0.05), (p[1]-0.05, p[1]+0.05), (p[2]-0.08, p[2]+0.05)]
+# cloud | single object
+# p = [-0.09, 0.61, -0.15]
+# grasp_space = [(p[0]-0.05, p[0]+0.05), (p[1]-0.05, p[1]+0.05), (p[2]-0.08, p[2]+0.05)]
+
+# cloud2
+grasp_space = [(-0.4, 0.25), (0.68, 1.5), (-0.225, 0.50)]
 
 grasp_offsets = [0.23, 0, 0.16]
 grasp_offsets[1] = grasp_offsets[2] + (grasp_offsets[0]-grasp_offsets[2])/2.0
@@ -39,17 +43,23 @@ rviz_node = RvizNode()
 plot_rviz = PlotRviz()
 plot_rviz.initRos()
 
-cloud = point_cloud.LoadPcd(os.path.dirname(__file__) + '/../data/cloud.pcd')
+cloud = point_cloud.LoadPcd(os.path.dirname(__file__) + '/../data/cloud3.pcd')
 cloud = point_cloud_util.filter_workspace(workspace, cloud)
 
-grasp_cloud = point_cloud_util.filter_workspace(grasp_space, cloud)
+# grasp_cloud = point_cloud_util.filter_workspace(grasp_space, cloud)
+grasp_cloud = cloud
 grasps = grasp_proxy.detect_grasps_whole_cloud(grasp_cloud, grasp_offsets)
+grasps = filter(lambda g: (grasp_space[0][0] < g.bottom[0] < grasp_space[0][1] and
+                           grasp_space[1][0] < g.bottom[1] < grasp_space[1][1] and
+                           grasp_space[2][0] < g.bottom[2] < grasp_space[2][1]),
+                grasps)
 # grasps = filter(lambda g: g.bottom[1] < 0.55, grasps)
 rviz_node.cloud_pub.publish(cloud_proxy.convert_to_point_cloud2(cloud))
 rviz_node.all_grasps_pub.publish(plot.createGraspsMarkerArray(grasps, rgba=[1, 0, 0, 0.5]))
 rviz_node.grasp_space_pub.publish(plot.createGraspsPosCube(grasp_space))
 
 array = []
+
 for grasp in grasps:
     test_hand_des = HandDescriptor(HandDescriptor.T_from_approach_axis_center(grasp.approach, grasp.axis,
                                                                               (grasp.bottom+grasp.top)/2))
@@ -66,6 +76,7 @@ try:
 finally:
     # np.savetxt(f, array.reshape([array.shape[0], -1]))
     # np.save(f, array.reshape([array.shape[0], -1]))
+    raw_input('write?')
     np.save(f, array)
 
 aaa = 1
